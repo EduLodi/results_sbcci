4226f2fe723707da8913dbd2f2b1765d7e998119
edulodi <f.eduardo.lodi@gmail.com>
2023-04-16 22:37:19 -0300

    added heatmaps to reults

--- presets_plots.py
+++ presets_plots.py
@@ -1,6 +1,10 @@
-import pandas as pd
-import matplotlib.pyplot as plt
 import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+from CodecComparator import CodecComparator
 
 presetsvvenc8t = ["faster","fast","medium","slow"]
 presetssvt8t = ["0","3","6","9","12"]
@@ -39,11 +43,150 @@ color_dict = {
     "evcplacebo": "#606060",
 }
 
+def baseline_list(codecname, videoname, metric):
+    # Define dictionary with codec and video names as keys, and corresponding lists as values
+    codec_video_dict = {
+        ("vvcodec","icecif","bitrate"): [727190.528, 362350.592, 192218.112, 107209.728] ,
+        ("vvcodec","icecif","psnr"): [46.9683, 43.4699, 40.0982, 36.9726] ,
+        ("vvcodec","ice4cif","bitrate"): [3166971.904, 1159593.984, 531959.808, 278302.72] ,
+        ("vvcodec","ice4cif","psnr"): [45.6675, 43.3623, 40.9889, 38.5962] ,
+        ("vvcodec","harbourcif","bitrate"): [1337489.8176, 609258.7008, 265990.144, 123230.6176] ,
+        ("vvcodec","harbourcif","psnr"): [38.9481, 35.6, 32.6099, 30.2122] ,
+        ("vvcodec","harbour4cif","bitrate"): [8075815.3216, 3143208.1408, 1331169.6896, 616856.7808] ,
+        ("vvcodec","harbour4cif","psnr"): [40.2989, 37.5237, 35.0531, 32.8381] ,
+        ("vvcodec","duckstakeoff720p","bitrate"): [29805701.5296, 10797395.1488, 4124372.992, 1686014.3616] ,
+        ("vvcodec","duckstakeoff720p","psnr"): [36.2287, 32.4808, 29.7488, 27.6533] ,
+        ("vvcodec","duckstakeoff1080p","bitrate"): [63115795.6608, 19868524.544, 7250622.0544, 2884089.4464] ,
+        ("vvcodec","duckstakeoff1080p","psnr"): [35.6887, 32.528, 30.1386, 28.1981] ,
+        ("vvcodec","duckstakeoff2160p","bitrate"): [265424645.3248, 48342090.5472, 15735205.0688, 6392722.2272] ,
+        ("vvcodec","duckstakeoff2160p","psnr"): [34.3212, 31.9036, 30.6213, 29.2868] ,
+        ("vvcodec","parkjoy720p","bitrate"): [18337358.6432, 7681733.4272, 3093372.1088, 1229819.0848] ,
+        ("vvcodec","parkjoy720p","psnr"): [35.0248, 31.4729, 28.4818, 26.1014] ,
+        ("vvcodec","parkjoy1080p","bitrate"): [34663750.8608, 14162034.688, 5548389.9904, 2215502.6432] ,
+        ("vvcodec","parkjoy1080p","psnr"): [35.4028, 32.089, 29.2126, 26.9204] ,
+        ("vvcodec","parkjoy2160p","bitrate"): [70836982.5792, 28791624.4992, 11649017.4464, 4856461.7216] ,
+        ("vvcodec","parkjoy2160p","psnr"): [36.2686, 34.1175, 31.7909, 29.6272] ,
+        ("evc","icecif","bitrate"): [576568.32, 292905.984, 158708.736, 86924.288] ,
+        ("evc","icecif","psnr"): [43.3717, 39.8224, 36.8859, 34.2036] ,
+        ("evc","ice4cif","bitrate"): [2087734.272, 954927.104, 500894.72, 270683.136] ,
+        ("evc","ice4cif","psnr"): [43.8201, 41.3726, 39.1092, 36.7659] ,
+        ("evc","harbourcif","bitrate"): [1643282.432, 624069.8368, 248369.9712, 96517.3248] ,
+        ("evc","harbourcif","psnr"): [38.8706, 35.1345, 32.3728, 30.0745] ,
+        ("evc","harbour4cif","bitrate"): [8505440.6656, 2856333.312, 1154031.616, 468562.7392] ,
+        ("evc","harbour4cif","psnr"): [39.9076, 36.8459, 34.4697, 32.1926] ,
+        ("evc","duckstakeoff720p","bitrate"): [42629201.92, 16264867.0208, 6492803.072, 2510621.9008] ,
+        ("evc","duckstakeoff720p","psnr"): [36.7911, 33.0203, 30.4095, 28.2446] ,
+        ("evc","duckstakeoff1080p","bitrate"): [95900976.7424, 32610745.5488, 12726935.552, 4933585.3056] ,
+        ("evc","duckstakeoff1080p","psnr"): [36.2436, 33.0097, 30.7702, 28.8403] ,
+        ("evc","duckstakeoff2160p","bitrate"): [467314223.5136, 97486935.6544, 31631229.7472, 13442486.272] ,
+        ("evc","duckstakeoff2160p","psnr"): [35.4554, 32.2463, 31.073, 29.8705] ,
+        ("evc","parkjoy720p","bitrate"): [37466099.712, 15097083.4944, 6352027.648, 2446330.2656] ,
+        ("evc","parkjoy720p","psnr"): [36.9685, 32.7517, 29.8518, 27.4184] ,
+        ("evc","parkjoy1080p","bitrate"): [73260928.2048, 29256412.3648, 12274248.9088, 4839632.896] ,
+        ("evc","parkjoy1080p","psnr"): [37.1292, 33.2858, 30.5056, 28.1429] ,
+        ("evc","parkjoy2160p","bitrate"): [183618585.3952, 63464698.6752, 27505357.6192, 12059389.952] ,
+        ("evc","parkjoy2160p","psnr"): [37.4008, 34.8886, 32.7601, 30.6935] ,
+        ("svt","icecif","bitrate"): [538644.48, 433909.76, 337530.88, 259747.84] ,
+        ("svt","icecif","psnr"): [46.24, 45.13, 43.85, 42.52] ,
+        ("svt","ice4cif","bitrate"): [2480046.08, 1798164.48, 1317437.44, 963287.04] ,
+        ("svt","ice4cif","psnr"): [46.1, 45.36, 44.57, 43.7] ,
+        ("svt","harbourcif","bitrate"): [1892259.84, 1364705.28, 955074.56, 655943.68] ,
+        ("svt","harbourcif","psnr"): [41.2, 39.8, 38.43, 37.13] ,
+        ("svt","harbour4cif","bitrate"): [11276564.48, 7566018.56, 4829153.28, 3129057.28] ,
+        ("svt","harbour4cif","psnr"): [41.85, 40.66, 39.49, 38.41] ,
+        ("svt","duckstakeoff720p","bitrate"): [52453468.16, 36496711.68, 25128284.16, 16632832.0] ,
+        ("svt","duckstakeoff720p","psnr"): [38.92, 37.27, 35.77, 34.3] ,
+        ("svt","duckstakeoff1080p","bitrate"): [128529602.56, 82443796.48, 52842526.72, 33534832.64] ,
+        ("svt","duckstakeoff1080p","psnr"): [38.46, 36.7, 35.35, 34.11] ,
+        ("svt","duckstakeoff2160p","bitrate"): [686617108.48, 431365273.6, 238725642.24, 109476239.36] ,
+        ("svt","duckstakeoff2160p","psnr"): [38.19, 35.98, 34.25, 32.94] ,
+        ("svt","parkjoy720p","bitrate"): [41975142.4, 31740938.24, 22450452.48, 15673845.76] ,
+        ("svt","parkjoy720p","psnr"): [39.53, 37.98, 36.23, 34.62] ,
+        ("svt","parkjoy1080p","bitrate"): [85469358.08, 62882570.24, 43853557.76, 30346905.6] ,
+        ("svt","parkjoy1080p","psnr"): [39.36, 38.0, 36.49, 35.02] ,
+        ("svt","parkjoy2160p","bitrate"): [257051463.68, 159863398.4, 97251061.76, 64592599.04] ,
+        ("svt","parkjoy2160p","psnr"): [39.14, 37.98, 36.95, 36.06]
+    }
+
+    # Get the corresponding list from the dictionary based on input codec and video names
+    try:
+        list_to_return = codec_video_dict[(codecname, videoname, metric)]
+    except KeyError:
+        print("Invalid codec or video name.")
+        return None
+
+    return list_to_return
+
+def heatplot(videoname, threads, codeclist, timelist, bdratelist, bdpsnrlist, basecodec):
+    # Create a dictionary with your data
+    data = {'codec_config': codeclist,
+            'time': timelist,
+            'bdrate': bdratelist,
+            'bdpsnr': bdpsnrlist}
+    pathtoresults = os.path.join(os.path.expanduser("~"),"videocoding","results_sbcci","initialresultssbcci")
+    threadpath = thread+"hread"
+    if "EVC" in basecodec:
+        basecodecpath = "vsEVC"
+    if "VVenC" in basecodec:
+        basecodecpath = "vsVVCODEC"
+    if "SVT" in basecodec:
+        basecodecpath = "vsSVT"
+    figname = videoname+".png"
+    fullsavepath = os.path.join(pathtoresults,threadpath,"heatBDRATE",basecodecpath,figname)
+
+    # Convert the dictionary to a Pandas DataFrame
+    df = pd.DataFrame.from_dict(data)
+
+    # Reshape the DataFrame to a pivot table format
+    table = df.pivot(index='time', columns='codec_config', values='bdrate')
+
+    # Create a heatmap with seaborn
+    plt.figure(figsize=(13, 7))
+    ax = sns.heatmap(table, cmap='coolwarm', annot=True, fmt=".2f")
+
+    # Add labels for the x and y axes
+    ax.set_xlabel('Codec and preset')
+    ax.set_ylabel('Time (s)')
+    ax.set_title("Heatmap of BD-rate(%) and time for "+videoname+" encoded with " + threads[:-1] + " threads")
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=25, ha='right')
+    ax.set_yticklabels(ax.get_yticklabels(), rotation=0)
+    plt.suptitle("Using "+basecodec+" as baseline for BD-rate calculation")
+    # Show the plot
+    plt.savefig(fullsavepath ,dpi=200)
+
+    fullsavepath = os.path.join(pathtoresults,threadpath,"heatBDPSNR",basecodecpath,figname)
+
+    table = df.pivot(index='time', columns='codec_config', values='bdpsnr')
+
+    # Create a heatmap with seaborn
+    plt.figure(figsize=(13, 7))
+    ax = sns.heatmap(table, cmap='coolwarm', annot=True, fmt=".2f")
+
+    # Add labels for the x and y axes
+    ax.set_xlabel('Codec and preset')
+    ax.set_ylabel('Time (s)')
+    ax.set_title("Heatmap of BD-PSNR(dB) and time for "+videoname+" encoded with " + threads[:-1] + " threads")
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=25, ha='right')
+    ax.set_yticklabels(ax.get_yticklabels(), rotation=0)
+    plt.suptitle("Using "+basecodec+" as baseline for BD-PSNR calculation")
+    # Show the plot
+    plt.savefig(fullsavepath,dpi=200)
 
 psnrlist = []
 bitratelist = []
 timelist = []
 vmaflist = []
+
+codecconfig = []
+timeconfig = []
+bdratesvt = []
+bdpsnrsvt = []
+bdratevvcodec = []
+bdpsnrvvcodec = []
+bdrateevc = []
+bdpsnrevc = []
+
+comp = CodecComparator()
 
 mode = "BRxTIME"
 
@@ -81,7 +224,7 @@ for thread in ["1t", "4t", "8t"]:
                 folder_path_csv = os.path.join("output",codec,preset,"csv",video)
                 #csv_path_vmaf = os.path.join("output",codec,"metrics","VMAF",video,"averageVMAFs","pathavg_vmaf.csv")
 
-                for qp in ["17qp","22qp","27qp","32qp","37qp","42qp"]:
+                for qp in ["22qp","27qp","32qp","37qp"]:
                     
                     #df = pd.read_csv(csv_path_vmaf)
                     #mask = (df['name'].str.contains(qp)) & (df['name'].str.contains("_"+preset+"-preset"))
@@ -92,56 +235,97 @@ for thread in ["1t", "4t", "8t"]:
                             if thread in filename:    
                                 if qp in filename:
                                     df = pd.read_csv(os.path.join(folder_path_csv, filename))
-                                    print(os.path.join(folder_path_csv, filename))
+                                    #print(os.path.join(folder_path_csv, filename))
                                     psnrlist.append(df.at[0, 'psnr'])
                                     bitratelist.append(df.at[0, 'bitrate'])
                                     timelist.append(df.at[0, "time(s)"])
+                                
+                avg_time = (timelist[0] + timelist[1] + timelist[2] + timelist[3]) / 4
+                
+                timeconfig.append(avg_time)
+                codecconfig.append((codec+"_"+preset))
+                
+                #print("BDRATE for "+video+" encoded with "+codec+" at preset "+preset+" with SVT0 as baseline: ")
+                brbaselist = baseline_list("svt",video,"bitrate")
+                psnrbaselist = baseline_list("svt",video,"psnr")
+                bdratesvt.append(comp.BD_RATE(brbaselist,psnrbaselist,bitratelist,psnrlist))
+                #print("BDPSNR for "+video+" encoded with "+codec+" at preset "+preset+" with SVT0 as baseline: ")
+                brbaselist = baseline_list("svt",video,"bitrate")
+                psnrbaselist = baseline_list("svt",video,"psnr")
+                bdpsnrsvt.append(comp.BD_PSNR(brbaselist,psnrbaselist,bitratelist,psnrlist))
+
+                #print("BDRATE for "+video+" encoded with "+codec+" at preset "+preset+" with vvcodec slow as baseline: ")
+                brbaselist = baseline_list("vvcodec",video,"bitrate")
+                psnrbaselist = baseline_list("vvcodec",video,"psnr")
+                bdratevvcodec.append(comp.BD_RATE(brbaselist,psnrbaselist,bitratelist,psnrlist))
+                #print("BDPSNR for "+video+" encoded with "+codec+" at preset "+preset+" with vvcodec slow as baseline: ")
+                brbaselist = baseline_list("vvcodec",video,"bitrate")
+                psnrbaselist = baseline_list("vvcodec",video,"psnr")
+                bdpsnrvvcodec.append(comp.BD_PSNR(brbaselist,psnrbaselist,bitratelist,psnrlist))
+
+                #print("BDRATE for "+video+" encoded with "+codec+" at preset "+preset+" with evc slow as baseline: ")
+                brbaselist = baseline_list("evc",video,"bitrate")
+                psnrbaselist = baseline_list("evc",video,"psnr")
+                bdrateevc.append(comp.BD_RATE(brbaselist,psnrbaselist,bitratelist,psnrlist))
+                #print("BDPSNR for "+video+" encoded with "+codec+" at preset "+preset+" with evc slow as baseline: ")
+                brbaselist = baseline_list("evc",video,"bitrate")
+                psnrbaselist = baseline_list("evc",video,"psnr")
+                bdpsnrevc.append(comp.BD_PSNR(brbaselist,psnrbaselist,bitratelist,psnrlist))               
 
 
-
-
-                color = color_dict[codec+preset]
-                if mode == "PSNRxBR":
-                    plt.plot(bitratelist, psnrlist, "o-", label=codec+preset, color = color, markersize = 5)
-                if mode == "PSNRxTIME":
-                    plt.plot(timelist, psnrlist, "o-", label=codec+preset, color = color, markersize = 5)    
-                if mode == "BRxTIME":
-                    plt.plot(timelist, bitratelist, "o-", label=codec+preset, color = color, markersize = 5)
-                if mode == "VMAFxTIME":
-                    plt.plot(timelist, vmaflist, "o-", label=codec+preset, color = color, markersize = 5)
-                if mode == "VMAFxBR":
-                    plt.plot(bitratelist,vmaflist, "o-", label=codec+preset, color = color, markersize = 5)
+                #color = color_dict[codec+preset]
+                #if mode == "PSNRxBR":
+                #    plt.plot(bitratelist, psnrlist, "o-", label=codec+preset, color = color, markersize = 5)
+                #if mode == "PSNRxTIME":
+                #    plt.plot(timelist, psnrlist, "o-", label=codec+preset, color = color, markersize = 5)    
+                #if mode == "BRxTIME":
+                #    plt.plot(timelist, bitratelist, "o-", label=codec+preset, color = color, markersize = 5)
+                #if mode == "VMAFxTIME":
+                #    plt.plot(timelist, vmaflist, "o-", label=codec+preset, color = color, markersize = 5)
+                #if mode == "VMAFxBR":
+                #    plt.plot(bitratelist,vmaflist, "o-", label=codec+preset, color = color, markersize = 5)
 
                 psnrlist = []
                 bitratelist = []
                 vmaflist = []
                 timelist = []
+                
+        timeconfig = [round(value,2) for value in timeconfig]
+        heatplot(video, thread, codecconfig, timeconfig, bdratesvt, bdpsnrsvt,"SVT 0 with 8 threads")
+        heatplot(video,thread, codecconfig, timeconfig, bdratevvcodec, bdpsnrvvcodec,"VVenC slow with 8 threads")
+        heatplot(video, thread, codecconfig, timeconfig, bdrateevc, bdpsnrevc, "EVC slow with 8 threads")
+        codecconfig=[]
+        timeconfig=[]
+        bdratesvt = []
+        bdpsnrsvt = []
+        bdratevvcodec = []
+        bdpsnrvvcodec = []
+        bdrateevc = []
+        bdpsnrevc = []
+        #if mode == "PSNRxBR":
+        #    plt.xlabel('Bitrate')
+        #    plt.ylabel('PSNR')
+        #    plt.title(video + " " + thread + ' PNSR vs Bitrate')
+        #if mode == "BRxTIME":
+        #    plt.xlabel('Time(s)')
+        #    plt.ylabel('Bitrate')
+        #    plt.title(video + " " + thread + ' PNSR vs Bitrate')
+        #if mode == "PSNRxTIME":
+        #    plt.xlabel('Time(s)')
+        #    plt.ylabel('PSNR')
+        #    plt.title(video + " " + thread + ' PNSR vs Bitrate')    
+        #if mode == "VMAFxTIME":
+        #    plt.xlabel('Time(s)')
+        #    plt.ylabel('VMAF score')
+        #    plt.title(video + ' VMAF vs Time')
+        #if mode == "VMAFxBR":
+        #    plt.xlabel('Bitrate')
+        #    plt.ylabel('VMAF score')
+        #    plt.title(video + ' VMAF vs Bitrate')
 
-
-        if mode == "PSNRxBR":
-            plt.xlabel('Bitrate')
-            plt.ylabel('PSNR')
-            plt.title(video + " " + thread + ' PNSR vs Bitrate')
-        if mode == "BRxTIME":
-            plt.xlabel('Time(s)')
-            plt.ylabel('Bitrate')
-            plt.title(video + " " + thread + ' PNSR vs Bitrate')
-        if mode == "PSNRxTIME":
-            plt.xlabel('Time(s)')
-            plt.ylabel('PSNR')
-            plt.title(video + " " + thread + ' PNSR vs Bitrate')    
-        if mode == "VMAFxTIME":
-            plt.xlabel('Time(s)')
-            plt.ylabel('VMAF score')
-            plt.title(video + ' VMAF vs Time')
-        if mode == "VMAFxBR":
-            plt.xlabel('Bitrate')
-            plt.ylabel('VMAF score')
-            plt.title(video + ' VMAF vs Bitrate')
-
-        plt.legend()
-        plt.grid()
-        plt.show()
+        #plt.legend()
+        #plt.grid()
+        #plt.show()
     
     psnrlist = []
     bitratelist = []
